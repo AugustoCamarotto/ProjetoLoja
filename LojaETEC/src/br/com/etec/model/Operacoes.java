215711305c81973191db4c0f66784e1f0ccecf82
lab04 <lab04@lab4-03>
2024-08-19 20:57:00 -0300

    Endereçamento de campos.
    *Campo de texto txfUsuario;
    *Campo de senha psfSenha;
    *Campo Botão acesso btnAcessar;
    *Campo Botão cadastro btnCadastrar;
    *Condição (if) de acesso;

--- LojaETEC/src/br/com/etec/model/Operacoes.java
+++ LojaETEC/src/br/com/etec/model/Operacoes.java
@@ -1,5 +1,29 @@
 package br.com.etec.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Operacoes {
+	
+	@FXML
+	private TextField txfUsuario;
+	@FXML
+	private PasswordField psfSenha;
+	@FXML
+	private Button btnAcessar;
+	@FXML
+	private Button btnCadastrar;
+	
+	@FXML
+	private void validarUsuario(ActionEvent event) {
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		String senhaUsuario;
+		senhaUsuario = psfSenha.getText();
+		
+	}
 	
 }
--- LojaETEC/src/module-info.java
+++ LojaETEC/src/module-info.java
@@ -1,6 +1,7 @@
 module LojaETEC {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens br.com.etec.model to javafx.graphics, javafx.fxml;
 	opens br.com.etec.controller to javafx.graphics, javafx.fxml;
