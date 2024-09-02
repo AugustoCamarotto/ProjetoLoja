0c465a2b99c03f62ff9e06fcbe1a71acbeb50e42
lab04 <lab04@lab4-03>
2024-09-02 20:44:50 -0300

    Adicionado Regra de Negócio da tela de login *Usuário: admin *Senha: admin Vinculado Ação(validarUsuario) com Botão Acessar

--- LojaETEC/src/br/com/etec/model/Operacoes.java
+++ LojaETEC/src/br/com/etec/model/Operacoes.java
@@ -2,6 +2,7 @@ package br.com.etec.model;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -23,7 +24,32 @@ public class Operacoes {
 		nomeUsuario = txfUsuario.getText();
 		String senhaUsuario;
 		senhaUsuario = psfSenha.getText();
-		
+			if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+				if(nomeUsuario.isEmpty()) {
+					mostrarAlerta(Alert.AlertType.WARNING, "INFORME UM NOME!", "É necessário colocar o nome de usuário.");
+				}else {
+					if(senhaUsuario.isEmpty()) {
+					mostrarAlerta(Alert.AlertType.WARNING, "INFORME SENHA!", "É necessário colocar uma senha.");
+					}
+				}
+			}
+			else {
+				if(nomeUsuario.equals("admin") && senhaUsuario.equals("admin")) {
+					mostrarAlerta(Alert.AlertType.CONFIRMATION, "ACESSO PERMITIDO!", "Login bem sucedido!");
+				}
+				else {
+					mostrarAlerta(Alert.AlertType.ERROR, "ACESSO NEGADO!", "Usuário ou Senha inválidos");
+				}
+			}
 	}
 	
+
+	
+	private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensagem) {
+		
+		Alert alerta = new Alert(tipo);
+		alerta.setTitle(titulo);
+		alerta.setContentText(mensagem);
+		alerta.showAndWait();
+	}
 }
