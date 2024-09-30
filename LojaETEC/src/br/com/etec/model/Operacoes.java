c18437e735e6fca134bbc9a3c3a41252377e2d46
lab04 <lab04@lab4-03>
2024-09-30 20:57:52 -0300

    Corrigido visual e adcionado funções:

--- LojaETEC/src/br/com/etec/model/Operacoes.java
+++ LojaETEC/src/br/com/etec/model/Operacoes.java
@@ -1,11 +1,17 @@
 package br.com.etec.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Operacoes {
 	
@@ -17,13 +23,19 @@ public class Operacoes {
 	private Button btnAcessar;
 	@FXML
 	private Button btnCadastrar;
-	
 	@FXML
-	private void validarUsuario(ActionEvent event) {
+	private Button btnFechar;
+	@FXML
+	private Stage acpPalco;
+	
+	//Botão Acessar!
+	@FXML
+	private void validarUsuario(ActionEvent event) throws SQLException {
 		String nomeUsuario;
 		nomeUsuario = txfUsuario.getText();
 		String senhaUsuario;
 		senhaUsuario = psfSenha.getText();
+		
 			if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
 				if(nomeUsuario.isEmpty()) {
 					mostrarAlerta(Alert.AlertType.WARNING, "INFORME UM NOME!", "É necessário colocar o nome de usuário.");
@@ -34,7 +46,7 @@ public class Operacoes {
 				}
 			}
 			else {
-				if(nomeUsuario.equals("admin") && senhaUsuario.equals("admin")) {
+				if(verificarUsuarioSenha(nomeUsuario, senhaUsuario)) {
 					mostrarAlerta(Alert.AlertType.CONFIRMATION, "ACESSO PERMITIDO!", "Login bem sucedido!");
 				}
 				else {
@@ -43,6 +55,32 @@ public class Operacoes {
 			}
 	}
 	
+	//Botão Cadastrar
+	@FXML
+	private void cadastrarUsuario(ActionEvent event) throws SQLException {
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		String senhaUsuario;
+		senhaUsuario = psfSenha.getText();
+		
+			if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+				if(nomeUsuario.isEmpty()) {
+					mostrarAlerta(Alert.AlertType.WARNING, "INFORME UM NOME!", "É necessário colocar o nome de usuário.");
+				}else {
+					if(senhaUsuario.isEmpty()) {
+					mostrarAlerta(Alert.AlertType.WARNING, "INFORME SENHA!", "É necessário colocar uma senha.");
+					}
+				}
+			}
+			else {
+				if(cadastrarUsuarioSenha(nomeUsuario, senhaUsuario)) {
+					mostrarAlerta(Alert.AlertType.CONFIRMATION, "Usuário Cadastrado!", "Cadastro Efetuado!");
+				}
+				else {
+					mostrarAlerta(Alert.AlertType.ERROR, "Tente Novamente!", "Erro No Cadastro");
+				}
+			}
+	}
 
 	
 	private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensagem) {
@@ -52,4 +90,72 @@ public class Operacoes {
 		alerta.setContentText(mensagem);
 		alerta.showAndWait();
 	}
+	
+	@FXML
+	private void fecharTelaLogin(ActionEvent event) {
+		acpPalco = (Stage) btnFechar.getScene().getWindow();
+		acpPalco.close();
+	}
+	
+	private boolean verificarUsuarioSenha(String usuario, String senha) throws SQLException {
+        Connection conexao = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        boolean usuarioValido = false;
+
+        try {
+            conexao = Conexao.conectar();
+            String sql = "SELECT * FROM tabelasenha WHERE usuario = ? AND senha = SHA2(?, 256)";
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, usuario);
+            stmt.setString(2, senha);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                usuarioValido = true;
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            Conexao.fechar(conexao);
+        }
+
+        return usuarioValido;
+    }
+	
+	private boolean cadastrarUsuarioSenha(String usuario, String senha) throws SQLException {
+	    Connection conexao = null;
+	    PreparedStatement stmt = null;
+	    boolean usuarioValido = false;
+
+	    try {
+	        conexao = Conexao.conectar();
+	        String sql = "INSERT INTO tabelasenha (usuario, senha) VALUES (?, SHA2(?, 256))";
+	        stmt = conexao.prepareStatement(sql);
+	        stmt.setString(1, usuario);
+	        stmt.setString(2, senha);
+	        
+	        // Execute o comando de inserção
+	        int linhasAfetadas = stmt.executeUpdate();
+
+	        // Verifica se a inserção foi bem-sucedida
+	        if (linhasAfetadas > 0) {
+	            usuarioValido = true;
+	            mostrarAlerta(Alert.AlertType.INFORMATION, "Cadastro Sucesso", "Usuário cadastrado com sucesso!");
+	        }
+	    } catch (SQLException e) {
+	        mostrarAlerta(Alert.AlertType.ERROR, "Erro no Cadastro", "Ocorreu um erro ao cadastrar o usuário: " + e.getMessage());
+	    } finally {
+	        if (stmt != null) {
+	            stmt.close();
+	        }
+	        Conexao.fechar(conexao);
+	    }
+	    return usuarioValido;
+	}
+
 }
--- LojaETEC/src/module-info.java
+++ LojaETEC/src/module-info.java
@@ -2,6 +2,8 @@ module LojaETEC {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires javafx.graphics;
+	requires java.sql;
 	
 	opens br.com.etec.model to javafx.graphics, javafx.fxml;
 	opens br.com.etec.controller to javafx.graphics, javafx.fxml;
