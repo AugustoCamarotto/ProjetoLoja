8759d4299b18aa2ce5819d400eb64743d2981d22
lab04 <lab04@lab4-03>
2024-11-04 20:46:53 -0300

    Alteraçoes Nos campos combo box ligando ao Banco de Dados

--- LojaETEC/src/br/com/etec/model/operacoesTelaPrincipal.java
+++ LojaETEC/src/br/com/etec/model/operacoesTelaPrincipal.java
@@ -1,6 +1,10 @@
 package br.com.etec.model;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -29,15 +33,37 @@ public class operacoesTelaPrincipal {
 	// Inicia os valores das combobox
 	public void initialize(URL url, ResourceBundle rb) {
 		
-		ObservableList<String> lista = FXCollections.observableArrayList("Tênis", "Chinelo");
-		cmbTipo.setItems(lista);
+		//ObservableList<String> lista = FXCollections.observableArrayList("Tênis", "Chinelo");
+		//cmbTipo.setItems(lista);
 		
-		ObservableList<String> lista2 = FXCollections.observableArrayList("Nike", "Adidas");
-		cmbMarca.setItems(lista2);
+		//ObservableList<String> lista2 = FXCollections.observableArrayList("Nike", "Adidas");
+		//cmbMarca.setItems(lista2);
 		
-		ObservableList<String> lista3 = FXCollections.observableArrayList("TEN001 (Jordan)",
-				"TEN002 (Shox)", "CHN001 (And 1)", "TEA001 (Swift Run)", "CHA001 (Adinova)");
-		cmbModelo.setItems(lista3);
+		//ObservableList<String> lista3 = FXCollections.observableArrayList("TEN001 (Jordan)",
+		//		"TEN002 (Shox)", "CHN001 (And 1)", "TEA001 (Swift Run)", "CHA001 (Adinova)");
+		//cmbModelo.setItems(lista3);
+		
+		carregarComboBox(cmbTipo, "tipo");
+		carregarComboBox(cmbMarca, "marca");
+		carregarComboBox(cmbModelo, "modelo");
 	}
 
+	private void carregarComboBox(ComboBox<String> comboBox, String coluna){
+		ObservableList<String> lista = FXCollections.observableArrayList();
+		
+		try (Connection conn = Conexao.conectar()){
+			
+			String sql = "SELECT DISTINCT" + coluna + "FROM tabelaprodutos";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				lista.add(rs.getString(coluna));
+			}
+			
+			comboBox.setItems(lista);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
